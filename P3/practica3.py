377bcd3edcd9c605e5816b75e83879da6840ea3d
Vladislav Nikolov <vladis890@gmail.com>
2019-05-04 02:05:03 +0200

    Continued P3.

--- P3/practica3.py
+++ P3/practica3.py
@@ -7,5 +7,25 @@ Autor: Vladislav Nikolov Vasilev
 import numpy as np
 import pandas as pd
 
-data = pd.read_csv('datos/optdigits.tra', header=None).values
+from sklearn.decomposition import PCA  # Reducir dimensiones
+from sklearn import preprocessing      # Escalar los datos
+
+sample = pd.read_csv('datos/optdigits.tra', header=None).values
+print(sample)
+
+# Obtener datos y etiquetas
+data = sample[:, :-1]
+labels = sample[:, -1]
+
+# Escalar los datos (restar la media de cada caracteristica y dividir entre desviacion tipica)
+data = preprocessing.scale(data)
+
 print(data)
+print(labels)
+
+pca = PCA(n_components=10)
+pca.fit(data)
+
+print(pca.explained_variance_ratio_)
+print(pca.explained_variance_)
+print('Componentes ', pca.components_.shape)
