3913568b6e2bd068270135ed6a2f09db1a60d950
Vladislav Nikolov <vladis890@gmail.com>
2019-05-10 14:08:22 +0200

    Added function to read data. Changed scaler to sklearn's StandardScaler.

--- P3/practica3.py
+++ P3/practica3.py
@@ -7,25 +7,55 @@ Autor: Vladislav Nikolov Vasilev
 import numpy as np
 import pandas as pd
 
-from sklearn.decomposition import PCA  # Reducir dimensiones
-from sklearn import preprocessing      # Escalar los datos
+from sklearn.decomposition import PCA               # Reducir dimensiones
+from sklearn.preprocessing import StandardScaler    # Escalar los datos
 
-sample = pd.read_csv('datos/optdigits.tra', header=None).values
-print(sample)
+def read_data_values(in_file, separator=None):
+    """
+    Funcion para leer los datos de un archivo
+    
+    :param in_file Archivo de entrada
+    :param separator Separador que se utiliza en el archivo (por defecto None)
+    
+    :return Devuelve los datos leidos del archivo
+    """
+    
+    # Cargar los datos en un DataFrame
+    # Se indica que la primera columna no es el header
+    df = pd.read_csv(in_file, sep=separator, header=None)
+    
+    # Obtener los valores del DataFrame
+    data = df.values
+    
+    return data
+
+
+
+# Lectura de los datos
+sample = read_data_values('datos/airfoil_self_noise.dat', separator='\t')
 
 # Obtener datos y etiquetas
 data = sample[:, :-1]
 labels = sample[:, -1]
 
+print(sample)
+
+print(data.shape)
+print(labels.shape)
+
 # Escalar los datos (restar la media de cada caracteristica y dividir entre desviacion tipica)
-data = preprocessing.scale(data)
+scaler = StandardScaler()
+scaler.fit(data)
+data = scaler.transform(data)
 
-print(data)
-print(labels)
 
-pca = PCA(n_components=10)
+
+pca = PCA(n_components=.90)
 pca.fit(data)
+data_trans = pca.transform(data)
 
-print(pca.explained_variance_ratio_)
-print(pca.explained_variance_)
+print('Ratios de varianza explicada: ', pca.explained_variance_ratio_.sum())
 print('Componentes ', pca.components_.shape)
+
+print(data_trans.shape)
+print(data_trans)
